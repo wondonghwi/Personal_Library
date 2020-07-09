e61d6d0d505fc5ebff47b50e12f545cb3a3644d8
원동휘 <ehdehd1289@naver.com>
2020-07-09 22:16:14 +0900

    fixed location error

--- js/login.js
+++ js/login.js
@@ -24,7 +24,7 @@ const login = async (e) => {
       return;
     }
     localStorage.setItem('token', token);
-    location = './';
+    location = '../';
   } catch (error) {
     const data = error.response.data;
     if (data) {
